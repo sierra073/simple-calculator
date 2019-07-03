0dfbe69dfd77e3098a9a27a230a7828d6cefa1ba
Sierra Costanza <costanza.sierra@gmail.com>
2019-07-03 14:10:28 -0700

    wip

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const add = (a, b) => a + b;
 const subtract = (a, b) => a - b;
 const multiply = (a, b) => a * b;
 const divide = (a, b) => a / b;
+const power = (a, b) => Math.pow(a, b);
 
 const result = document.querySelector('#result');
 const nums = document.querySelectorAll(".btn-num");
@@ -14,14 +15,17 @@ let secondOperand = '';
 let currOperator = '';
 let operated = false;
 
-result.textContent = 0;
+result.textContent = '0';
+
 nums.forEach((num) => num.addEventListener('click', () => numButtonHandler(num.textContent)));
 operators.forEach((op) => op.addEventListener('click', () => opButtonHandler(op.textContent)));
 misc.forEach((m) => m.addEventListener('click', () => miscButtonHandler(m.textContent)));
 
 const checkResize = () => {
   if (result.textContent.length >= 10) {
-    result.style.fontSize = 14.5 / (result.textContent.length*.58) + 'vw';
+    result.style.fontSize = 14.5 / (power(result.textContent.length,1.28)*0.35) + 'vw';
+  } else {
+    result.style.fontSize = '3vw';
   }
 }
 
@@ -29,33 +33,24 @@ function displayNum(num) {
   if (result.textContent==='0' && num != '0') {
     result.textContent = Number(num);
   } else {
-  	if (num.length < 10) {
+  	if (num.length < 10 || Number.isInteger(Number(num))) {
   		result.textContent = Number(num);
   	} else {
   		let fixed = '' + Number(num).toFixed(3);
-  		if (fixed.length < 10) {
-  			result.textContent = fixed;
-  		}
+  		result.textContent = fixed;
   	}
-   checkResize()
  }
+ checkResize()
 }
 
 function numButtonHandler(num) {
-  console.log(num);
-  console.log(firstOperand);
-  console.log(secondOperand);
   if (!operated) {
     firstOperand += num;
     displayNum(firstOperand);
   } else {
-    if (!secondOperand) {
-      secondOperand += num;
-    }
+    secondOperand += num;
     displayNum(secondOperand);
   }
-  console.log(firstOperand);
-  console.log(secondOperand);
 }
 
 function operate(op, a, b) {
@@ -70,24 +65,41 @@ function operate(op, a, b) {
       return divide(a, b);
   }
 }
-
+// 14 + 5 - (9 -- should be 19)
+// 12 - 3 + (15 -- should be 9)
+// 14 + 5 = 19 + 2 - (17 -- should be 21)
 function opButtonHandler(op) {
-  console.log(operated);
-  console.log(currOperator);
   operated = true;
+  console.log(firstOperand);
+  console.log(secondOperand);
   if (op != '=') {
     currOperator = op;
     if (secondOperand) {
       firstOperand = '' + operate(currOperator, Number(firstOperand), Number(secondOperand));
+      displayNum(firstOperand)
       secondOperand = ''
     }
   } else {
-    console.log('here');
-    console.log(operate(currOperator, Number(firstOperand), Number(secondOperand)));
-    res = '' + operate(currOperator, Number(firstOperand), Number(secondOperand));
-    displayNum(res);
-    firstOperand = res;
-    secondOperand = '';
+    if (secondOperand) {
+      firstOperand = '' + operate(currOperator, Number(firstOperand), Number(secondOperand));
+      displayNum(firstOperand);
+      secondOperand = ''; // repetitive
+    } else {
+      firstOperand = result.textContent;
+    }
     currOperator = '';
+  }
+}
+
+function miscButtonHandler(op) {
+  switch (op) {
+    case 'AC': {
+      displayNum('0');
+      firstOperand = '';
+      secondOperand = '';
+      currOperator = '';
+      operated = false;
+      break;
+    }
   }
 }
